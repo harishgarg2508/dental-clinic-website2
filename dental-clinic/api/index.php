fead07a3a429c40c3d89010b42b64f0e46690aed
Harish Garg <rishusharma123321@gmail.com>
2025-04-24 15:13:52 +0530

    new edition

--- dental-clinic/api/index.php
+++ dental-clinic/api/index.php
@@ -1,14 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Maintenance mode check
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Autoload
 require __DIR__ . '/../vendor/autoload.php';
 
+// Bootstrap Laravel and handle the request
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
